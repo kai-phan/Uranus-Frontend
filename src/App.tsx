feba4ee163da4003217580bfb3cb941325fddfa5
Quy Phan <quyphan@Quys-MacBook-Pro-2.local>
2023-07-06 22:00:35 +0700

    add button and taiwind config

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,30 @@
-import { useState } from 'react';
-
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React from 'react';
 
 import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
+import { Button } from 'src/components/common';
 
+function App() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <React.Fragment>
+      <div className="grid grid-cols-4 gap-6">
+        <div className="w-20 h-20 bg-primary-1"></div>
+        <div className="w-20 h-20 bg-primary-2"></div>
+        <div className="w-20 h-20 bg-primary-3"></div>
+        <div className="w-20 h-20 bg-primary-4"></div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="text-text-color-1 text-[22px]">
+        A messenger that is ahead of its time
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Button variant="primary">Small</Button>
+      <Button variant="secondary">Small</Button>
+      <Button variant="primary" size="medium">
+        Medium
+      </Button>
+      <Button variant="default" size="medium">
+        Medium
+      </Button>
+    </React.Fragment>
   );
 }
 
