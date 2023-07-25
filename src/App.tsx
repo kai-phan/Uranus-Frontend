bc4404e02d905de63f12ee76d4ea6168b1cf83ed
Quy Phan <quyphan@Quys-MacBook-Pro-2.local>
2023-07-25 21:50:18 +0700

    add react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import routerConfig from 'src/router.config';
 
+export const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      staleTime: 1000 * 10,
+    },
+    mutations: {
+      onSuccess: () => {
+        console.log('application mutation level');
+      },
+      onError(error) {
+        console.log(error);
+      },
+    },
+  },
+});
+
 function App() {
   return (
-    <RouterProvider
-      router={routerConfig}
-      fallbackElement={<div>...LOADING SCRIPTS</div>}
-    />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider
+        router={routerConfig}
+        fallbackElement={<div>...LOADING SCRIPTS</div>}
+      />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   );
 }
 
--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,22 +1,124 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 export type Props = {
   className?: string;
 };
 
-const Index: React.FC<Props> = () => {
-  const data = useLoaderData();
+const useTodo = ({ page }: { page: string }) => {
+  return useQuery({
+    queryKey: ['todos', { _page: page }],
+    queryFn: async ({ queryKey }) => {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos?${new URLSearchParams(
+          queryKey[1],
+        ).toString()}`,
+      );
 
-  return <React.Fragment>{JSON.stringify(data)}</React.Fragment>;
+      return await res.json();
+    },
+    staleTime: 0,
+  });
+};
+
+const useUpdateTodo = ({ onSuccess }: { onSuccess: (data: any) => any }) => {
+  return useMutation<unknown, unknown, Todo>({
+    mutationFn: async (todo) => {
+      const res = await fetch(
+        // @ts-ignore
+        `https://jsonplaceholder.typicode.com/todos/${todo.id}`,
+        {
+          method: 'PUT',
+          body: JSON.stringify(todo),
+        },
+      );
+
+      return await res.json();
+    },
+    onSuccess: async (data) => {
+      await onSuccess(data);
+    },
+  });
+};
+
+type Todo = {
+  id: string;
+  userId: string;
+  title: string;
+  completed: boolean;
+};
+
+const Index: React.FC<Props> = () => {
+  const queryClient = useQueryClient();
+  const [page, setPage] = React.useState<string>('1');
+
+  const { data: todoList, isLoading: isLoadingList } = useTodo({ page });
+
+  const { mutate, isLoading: isUpdatingTodo } = useUpdateTodo({
+    onSuccess: () => {
+      queryClient.setQueryData(['todos', { _page: page }], (data) => {});
+    },
+  });
+
+  const [id, setId] = useState<string>();
+
+  const { data: todo } = useQuery<Todo>({
+    queryKey: ['todos', id],
+    queryFn: async ({ queryKey }) => {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/todos/${queryKey[1]}`,
+      );
+
+      return await res.json();
+    },
+    enabled: !!id,
+  });
+
+  return (
+    <React.Fragment>
+      <section className="grid grid-cols-2">
+        <div>
+          {isLoadingList ? (
+            <div>loading...</div>
+          ) : (
+            todoList?.map((d) => {
+              return (
+                <div key={d.id} onClick={() => setId(d.id)}>
+                  {d.id} - {d.title}
+                </div>
+              );
+            })
+          )}
+        </div>
+        {todo && (
+          <div>
+            <div>Todo id: {todo.userId}</div>
+            <div>Todo title: {todo.title}</div>
+            <div>Todo completed: {todo.completed}</div>
+
+            <br />
+            <button onClick={() => mutate({ ...todo, title: 'adfads' })}>
+              {isUpdatingTodo ? 'Updating' : ''} Update
+            </button>
+          </div>
+        )}
+      </section>
+      <br />
+      {[1, 2, 3, 4, 5].map((page) => {
+        return (
+          <button
+            className="bg-primary-1 p-4"
+            key={page}
+            onClick={() => setPage(page.toString())}
+          >
+            {page}
+          </button>
+        );
+      })}
+
+      <br />
+    </React.Fragment>
+  );
 };
 
 export const Component = Index;
-
-export function loader() {
-  return fetch('https://jsonplaceholder.typicode.com/todos/1').then((res) => {
-    if (res.ok) return res.json();
-
-    throw new Response(res.statusText, { status: res.status });
-  });
-}
